b7d08cbc75d68176bcf682d2a16c2dc74bf7274d
MrsCf28 <rebeccaflynn28@outlook.com>
2022-12-01 21:06:49 +0000

    day 1 complete

--- src/main/kotlin/2022-day1.kt
+++ src/main/kotlin/2022-day1.kt
@@ -1,5 +1,19 @@
-private val input = readResourceFileAsLines("2022-day1.txt").map { it.toInt() }
+private val input = readResourceFileAsLines("2022-day1.txt").map { it.toIntOrNull() }
 
 fun main() {
-
+    val calories: MutableList<Int> = mutableListOf()
+    var currentElf: Int = 0
+    for (item in input) {
+        if (item != null) {
+            currentElf += item
+        } else {
+            calories.add(currentElf)
+            currentElf = 0
+        }
+    }
+    val caloriesDesc: List<Int> = calories.sortedDescending()
+    val topCarrier = caloriesDesc[0]
+    val top3Carriers = caloriesDesc[0] + caloriesDesc[1] + caloriesDesc[2]
+    println(topCarrier)
+    println(top3Carriers)
 }

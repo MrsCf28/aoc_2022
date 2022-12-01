38e0a32548dc4ebac8a97d79552f612a902bbbb0
MrsCf28 <rebeccaflynn28@outlook.com>
2022-12-01 15:02:28 +0000

    remove build

---
+++ src/main/kotlin/2022-day1.kt
@@ -0,0 +1,5 @@
+private val input = readResourceFileAsLines("2022-day1.txt").map { it.toInt() }
+
+fun main() {
+
+}
---
+++ src/main/kotlin/utils.kt
@@ -0,0 +1,2 @@
+fun readResourceFile(name: String) = object {}::class.java.classLoader.getResource(name)!!.readText()
+fun readResourceFileAsLines(name: String) = readResourceFile(name).lines()

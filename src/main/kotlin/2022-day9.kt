835d98839f743965529c7e1e291372f22cb8d92a
MrsCf28 <rebeccaflynn28@outlook.com>
2022-12-10 01:20:29 +0000

    day 9 updated

--- src/main/kotlin/2022-day9.kt
+++ src/main/kotlin/2022-day9.kt
@@ -1,14 +1,11 @@
 import kotlin.math.abs
+import kotlin.math.sign
 
 private val input = readResourceFileAsLines("2022-day9.txt")
 
 fun main() {
-    val h = Coordinate(0, 0)
-    val middle = mutableListOf<Coordinate>()
-    repeat(8) {
-        middle.add(Coordinate(0,0))
-    }
-    val t = Coordinate(0,0)
+    val head = Coordinate(0, 0)
+    val tail = MutableList(9) { Coordinate(0, 0) }
 
     val tailPositions = mutableSetOf<Coordinate>()
     val tailPositions2 = mutableSetOf<Coordinate>()
@@ -16,15 +13,16 @@ fun main() {
     for (line in input) {
         val (direction, amount) = line.split(" ")
         repeat(amount.toInt()) {
-            h.move(direction)
-            middle[0].moveNext(h)
-            for (i in 1 .. 7) {
-                middle[i].moveNext(middle[i - 1])
+            head.move(direction)
+            for (i in tail.indices) {
+                if (i == 0) {
+                    tail[0].moveNext(head)
+                } else {
+                    tail[i].moveNext(tail[i - 1])
+                }
             }
-            t.moveNext(middle[7])
-            tailPositions.add(Coordinate(middle[0].x, middle[0].y))
-            tailPositions2.add(Coordinate(t.x, t.y))
-
+            tailPositions.add(Coordinate(tail[0].x, tail[0].y))
+            tailPositions2.add(Coordinate(tail[8].x, tail[8].y))
         }
     }
 
@@ -47,44 +45,18 @@ data class Coordinate(var x: Int, var y: Int) {
         val xDiff = prev.x - this.x
         val yDiff = prev.y - this.y
 
-        if (xDiff == 0 || yDiff == 0) {
+        if (xDiff * yDiff == 0) {
             if (abs(xDiff) == 2) {
-                this.x += xDiff / 2
+                this.x += xDiff.sign
             } else if (abs(yDiff) == 2) {
-                this.y += yDiff / 2
+                this.y += yDiff.sign
             }
-        } else if (xDiff * yDiff == 2) {
-            if (xDiff > 0) {
-                this.x++
-                this.y++
-            } else {
-                this.x--
-                this.y--
-            }
-        } else if (xDiff * yDiff == -2) {
-            if (xDiff > 0) {
-                this.x++
-                this.y--
-            } else {
-                this.x--
-                this.y++
-            }
-        } else if (xDiff * yDiff == 4) {
-            if (xDiff > 0) {
-                this.x++
-                this.y++
-            } else {
-                this.x--
-                this.y--
-            }
-        } else if (xDiff * yDiff == -4) {
-            if (xDiff > 0) {
-                this.x++
-                this.y--
-            } else {
-                this.x--
-                this.y++
-            }
+        } else if (xDiff * yDiff == 2 || xDiff * yDiff == 4) {
+            this.x += xDiff.sign
+            this.y += yDiff.sign
+        } else if (xDiff * yDiff == -2 || xDiff * yDiff == -4) {
+            this.x += xDiff.sign
+            this.y -= xDiff.sign
         }
     }
 }
